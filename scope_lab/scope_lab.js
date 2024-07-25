7d2a08e3311c34dc700e60dc06d1747a802446b9
Tenlive1 <nguyenperry99@gmail.com>
2024-07-25 19:03:31 -0400

    save

---
+++ scope_lab/scope_lab.js
@@ -0,0 +1,13 @@
+//global scope
+
+var globalVar = "i'm a global variable"
+let globallet = "i'm also global, but scoped with let";
+const globalconst = "i'm a global constant";
+
+
+{
+    //block scope
+    var blockvar = "blockvar";
+    let blocklet = "blocklet";
+    const blockconst= "blockconst";
+}
