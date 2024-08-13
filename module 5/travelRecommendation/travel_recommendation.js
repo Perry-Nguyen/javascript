c32bd60ffbeef14b4fb99a328eb7cbba9e4cdd4e
Tenlive1 <nguyenperry99@gmail.com>
2024-08-13 02:47:21 -0500

    done

--- module 5/travelRecommendation/travel_recommendation.js
+++ module 5/travelRecommendation/travel_recommendation.js
@@ -2,6 +2,7 @@ const clearbutton = document.getElementById("btnClear");
 const input = document.getElementById("Search");
 const searchbutton = document.getElementById("btnSearch");
 const search_container = document.getElementById('search_container');
+search_container.style.height ="50%";
 
 function clear(){
     input.value = "";
@@ -11,7 +12,7 @@ function clear(){
 clearbutton.addEventListener("click",clear);
 
 function search(){
-
+    search_container.innerHTML = "";
     const keyword  = ["beaches","temples","countries"]
 
     keyword.forEach(element => {
@@ -35,7 +36,7 @@ function search(){
                         data.beaches.forEach(element => {
                             const imgdiv = document.createElement('div');
                             imgdiv.id = 'search_img';
-                            imgdiv.innerHTML += `<img src="${element.imageUrl}" alt="hjh">`;
+                            imgdiv.innerHTML += `<img src="${element.imageUrl}" alt="hjh" style="width: 300px; height: 200px; filter: none;">`;
 
                             const contentdiv = document.createElement('div');
                             contentdiv.innerHTML += `<h1> ${element.name}</h1>`;
@@ -48,10 +49,41 @@ function search(){
                         });
                         break;
                     case 'temples':
+                        data.temples.forEach(element => {
+                            const imgdiv = document.createElement('div');
+                            imgdiv.id = 'search_img';
+                            imgdiv.innerHTML += `<img src="${element.imageUrl}" alt="hjh" style="width: 300px; height: 200px; filter: none;">`;
 
+                            const contentdiv = document.createElement('div');
+                            contentdiv.innerHTML += `<h1> ${element.name}</h1>`;
+                            contentdiv.innerHTML += `<p> ${element.description}</p>`;
+                            contentdiv.innerHTML += `<button> visit </button>`;
+                            
+
+                            search_container.appendChild(imgdiv);
+                            search_container.appendChild(contentdiv);
+                        });
                         break;
                     case 'countries':
+                        data.countries.forEach(element => {
+                            const titlediv = document.createElement('div');
+                            titlediv.id = "countries_names";
+                            titlediv.innerHTML += `<h1>${element.name}</h1>`
+                            search_container.appendChild(titlediv);
 
+                            element.cities.forEach(city => {
+                                const imgdiv = document.createElement('div');
+                                imgdiv.id = 'search_img';
+                                imgdiv.innerHTML += `<img src="${city.imageUrl}" alt="hjh" style="width: 300px; height: 200px; filter: none;">`;
+    
+                                const contentdiv = document.createElement('div');
+                                contentdiv.innerHTML += `<h2> ${city.name}</h2>`;
+                                contentdiv.innerHTML += `<p> ${city.description}</p>`;
+                                contentdiv.innerHTML += `<button> visit </button>`;
+                                search_container.appendChild(imgdiv);
+                                search_container.appendChild(contentdiv);
+                            })
+                        });
                         break;
                 }
             })
